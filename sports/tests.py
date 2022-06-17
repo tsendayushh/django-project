2d703243f99c6ff83024fdecb775f3fcbd1b1dac
tsendayushh <tsendayushh@gmail.com>
2022-06-17 13:53:52 +0800

    (A-3) sports with no players function, test

--- sports/tests.py
+++ sports/tests.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.test import TestCase
 
 from sports.models import Players, Sports
-from sports.views import get_sports_with_multiplayers
+from sports.views import get_sports_with_multiplayers, get_sports_with_no_player
 
 # Create your tests here.
 class SportsTests(TestCase):
@@ -11,16 +11,17 @@ class SportsTests(TestCase):
         for i in range(3):
             Players.objects.create(email=f'player{i}@test.com')
 
-        Sports.objects.create(name="Test1")
-        Sports.objects.create(name="Test2")
-
-
-    def test_animals_can_speak(self):
-        sport1 = Sports.objects.get(name="Test1")
+        sport1 = Sports.objects.create(name="Test1")
         sport1.players.set = Players.objects.all()
+        # sport1.save()
+        sport2 = Sports.objects.create(name="Test2")
+        # sport2.save()
 
-        sport2 = Sports.objects.get(name="Test2")
 
-
+    def test_multiplayer_sport_retriever(self):
         _sports = get_sports_with_multiplayers()
+        self.assertTrue(isinstance(_sports, QuerySet))
+
+    def test_noplayer_sport_retriever(self):
+        _sports = get_sports_with_no_player()
         self.assertTrue(isinstance(_sports, QuerySet))
--- sports/views.py
+++ sports/views.py
@@ -13,6 +13,21 @@ def get_sports_with_multiplayers(count=2):
     print(_queryset)
 
     # printing raw query of the above queryset
-    print(_queryset.query)
+    print('multiplayers sports query:',_queryset.query)
+
+    return _queryset
+
+
+def get_sports_with_no_player():
+    # This function retrieves sports that no player is associated with
+    """
+    param: None
+    returns: queryset
+    """
+    _queryset = Sports.objects.annotate(player_count=Count('players')).filter(player_count=0)
+    print(_queryset)
+
+    # printing raw query of the above queryset
+    print('sports with no players query:',_queryset.query)
 
     return _queryset
